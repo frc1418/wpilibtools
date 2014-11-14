57a161ca5913ca733f7c43478525726a2cd3f506
Fredric Silberberg <fredric@fsilberberg.com>
2014-11-14 14:25:24 -0500

    Updated crosscompile to use the frc toolchain

--- udpee/crosscompile.sh
+++ udpee/crosscompile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir -p build/arm
-arm-none-linux-gnueabi-g++ -O3 -g0 -Wall -c -fmessage-length=0 -o build/arm/udpee.o udpee.cpp
-arm-none-linux-gnueabi-g++ -Wl,-rpath-link -Xlinker -export-dynamic -o udpee_arm build/arm/udpee.o
+arm-frc-linux-gnueabi-g++ -O3 -g0 -Wall -c -fmessage-length=0 -o build/arm/udpee.o udpee.cpp
+arm-frc-linux-gnueabi-g++ -Wl,-rpath-link -Xlinker -export-dynamic -o udpee_arm build/arm/udpee.o
