3ba191a7168d99a651dad9afdd5b795a952f244f
Fredric Silberberg <fredric@fsilberberg.com>
2014-12-26 14:53:13 -0500

    Updated SuccessController to the new framework. As part of this, the JRE.tar.gz and the md5 are no longer deleted, and there is no option to keep the other files around.

--- java-installer/src/main/java/edu/wpi/first/wpilib/javainstaller/controllers/SuccessController.java
+++ java-installer/src/main/java/edu/wpi/first/wpilib/javainstaller/controllers/SuccessController.java
@@ -1,5 +1,6 @@
 package edu.wpi.first.wpilib.javainstaller.controllers;
 
+import edu.wpi.first.wpilib.javainstaller.Arguments;
 import edu.wpi.first.wpilib.javainstaller.MainApp;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -13,64 +14,41 @@ import javafx.scene.layout.BorderPane;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.security.auth.login.FailedLoginException;
 import java.io.File;
 import java.io.IOException;
 
 /**
  * Final controller for a successful deploy!
  */
-public class SuccessController {
-
-    @FXML
-    private BorderPane mainView;
+public class SuccessController extends AbstractController {
 
     @FXML
     private ImageView logoImageView;
 
-    @FXML
-    private CheckBox cleanupCheckBox;
-
-    private String m_jreFolder;
-    private String m_tarLocation;
-    private String m_untarredLocation;
-
     private final Logger m_logger = LogManager.getLogger();
 
-    @FXML
-    private void initialize() {
+    public SuccessController() {
+        super(false, Arguments.Controller.SUCCESS_CONTROLLER);
+    }
+
+    @Override
+    protected void initializeClass() {
         Image frcImage = new Image(getClass().getResourceAsStream("/images/FRCicon_RGB.jpg"));
         logoImageView.setImage(frcImage);
         logoImageView.setPreserveRatio(true);
     }
 
-    public void initialize(String jreFolder, String tarLocation, String untarredLocation) {
-        m_jreFolder = jreFolder;
-        m_tarLocation = tarLocation;
-        m_untarredLocation = untarredLocation;
-    }
-
     @FXML
     private void handleFinish(ActionEvent event) {
-        // Cleanup the installer debris
-        if (cleanupCheckBox.isSelected()) {
-            cleanupLocalFiles();
-        }
+        cleanupLocalFiles();
         m_logger.debug("Finished!");
         Platform.exit();
     }
 
     @FXML
     private void handleRestart(ActionEvent event) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/connect_roborio.fxml"));
-            Parent root = loader.load();
-            ConnectRoboRioController controller = loader.getController();
-//            controller.initialize(m_jreFolder, m_tarLocation, m_untarredLocation);
-            mainView.getScene().setRoot(root);
-        } catch (IOException e) {
-            m_logger.error("Error when loading intro_screen from restart");
-            MainApp.showErrorScreen(e);
-        }
+        moveNext(Arguments.Controller.CONNECT_ROBORIO_CONTROLLER);
     }
 
     /**
@@ -78,12 +56,9 @@ public class SuccessController {
      */
     private void cleanupLocalFiles() {
         m_logger.debug("Cleaning up installer files");
-        File roboRioJreFile = new File(DeployController.JRE_TGZ_NAME);
-        roboRioJreFile.delete();
-        File downloadedJreFile = new File(m_tarLocation);
-        downloadedJreFile.delete();
-        deleteDirectory(new File(m_untarredLocation));
-        deleteDirectory(new File(m_jreFolder));
+        new File(m_args.getArgument(Arguments.Argument.JRE_CREATOR_TAR)).delete();
+        deleteDirectory(new File(m_args.getArgument(Arguments.Argument.JRE_CREATOR_FOLDER)));
+        deleteDirectory(new File(m_args.getArgument(Arguments.Argument.JRE_FOLDER)));
     }
 
 
